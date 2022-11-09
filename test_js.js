888f5e5c7e00d66ae8c7b76d6035f9489f0bc6a5
Stanislav <gsv9026@mail.ru>
2022-11-09 16:06:16 +0300

    two task

--- test_js.js
+++ test_js.js
@@ -4,3 +4,13 @@
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
+
+
+
+//-----------------------------------------------------------------------------------------------------
+
+//Complete the regex petRegex to match the pets dog, cat, bird, or fish.
+
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/;
+let resul = petRegex.test(petString);
