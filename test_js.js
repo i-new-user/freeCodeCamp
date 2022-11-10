72a981e2e58a59ddcc8fa9a9c3c54308758875ad
Stanislav <gsv9026@mail.ru>
2022-11-10 09:33:20 +0300

    eight task

--- test_js.js
+++ test_js.js
@@ -66,6 +66,18 @@ let result6 = unRegex.test(exampleStr);
 //find all the vowels in the string quoteSample.Note: Be sure to match both upper- 
 //and lowercase vowels.
 
-let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let quoteSample1 = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 let vowelRegex = /[aeiou]/gi;
 let result7 = quoteSample.match(vowelRegex); 
+
+
+
+
+//---------------------------------------------------------------------------------------------------------------------
+
+//Match all the letters in the string quoteSample.
+//Note: Be sure to match both uppercase and lowercase letters.
+
+let quoteSample2 = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /a-z/gi; 
+let result = quoteSample.match(alphabetRegex);
