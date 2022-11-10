ccd71e4cd331d48db61f3b3f0594377b2ed51452
Stanislav <gsv9026@mail.ru>
2022-11-10 09:26:14 +0300

    seven task

--- test_js.js
+++ test_js.js
@@ -1,6 +1,5 @@
 //Apply the regex myRegex on the string myString using the .test() method.
 
-
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result1 = myRegex.test(myString);
@@ -17,16 +16,10 @@ let result2 = petRegex.test(petString);
 
 
 
-
-
-
 //-----------------------------------------------------------------------------------------
 
 //Write a regex fccRegex to match freeCodeCamp, no matter its case. Your 
 //regex should not match any abbreviations or variations with spaces.
-
-
-
 
 let myString2 = "freeCodeCamp";
 let fccRegex = /freeCodeCamp/i;
@@ -35,11 +28,9 @@ let result3 = fccRegex.test(myString);
 
 
 
-
 //----------------------------------------------------------------------------------------------------
 
 //Apply the .match() method to extract the string coding.
-
 
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/;
@@ -58,16 +49,23 @@ let result5 = twinkleStar.match(starRegex);
 
 
 
-
-
-
-
 //-----------------------------------------------------------------------------------------------------------------------------------
 
 //Complete the regex unRegex so that it matches the strings run, sun, fun, pun, nun, and bun.
 //Your regex should use the wildcard character.
  
-
 let exampleStr = "Let's have fun with regular expressions!";
 let unRegex = /.un/g;
 let result6 = unRegex.test(exampleStr);
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+//Use a character class with vowels (a, e, i, o, u) in your regex vowelRegex to 
+//find all the vowels in the string quoteSample.Note: Be sure to match both upper- 
+//and lowercase vowels.
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi;
+let result7 = quoteSample.match(vowelRegex); 
