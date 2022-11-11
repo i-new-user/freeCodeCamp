514dc7b01a6fdae478dcbce54658f838e1fa5ee0
Stanislav <gsv9026@mail.ru>
2022-11-11 11:00:29 +0300

    eighteen tasc

--- Regular_Expressions.js
+++ Regular_Expressions.js
@@ -214,3 +214,13 @@ let result17 = quoteSample.match(nonAlphabetRegex).length;
 
 
 
+
+
+//------------------------------------------------------------------------------------------------
+
+//Use the shorthand character class \d to count how many digits are in movie titles. 
+//Written out numbers ("six" instead of 6) do not count.
+
+let movieName = "2001: A Space Odyssey";
+let numRegex = /\d/g; 
+let result18 = movieName.match(numRegex).length;
