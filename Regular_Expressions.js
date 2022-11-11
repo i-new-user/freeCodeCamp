69503cbae012d82c96d000d20865873a7280d726
Stanislav <gsv9026@mail.ru>
2022-11-11 10:37:33 +0300

    fourteen tasc

--- Regular_Expressions.js
+++ Regular_Expressions.js
@@ -149,3 +149,10 @@ let result13 = text.match(myRegex);
 
 
 
+
+//----------------------------------------------------------------------------------------------------------------
+
+//Write a greedy regex that finds one or more criminals within a group of other people.
+//A criminal is represented by the capital letter C.
+
+let reCriminals = /C+/g;
