cd1e43bc3bc9ababda9139ccfe0c2b9f4086a5ee
Stanislav <gsv9026@mail.ru>
2022-11-14 11:24:41 +0300

    Generate Random Whole Numbers with JavaScript

--- Basic_JavaScript.js
+++ Basic_JavaScript.js
@@ -74,3 +74,14 @@ lookUpProfile("Akira", "likes");
 function randomFraction() {
   return Math.random();
 }
+
+
+
+
+
+
+//Generate Random Whole Numbers with JavaScript
+
+function randomWholeNum() {
+ return Math.floor( Math.random() * 10);
+}
