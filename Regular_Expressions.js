57430721d2358d1dadb751944de6c50bdbed41ac
Stanislav <gsv9026@mail.ru>
2022-11-14 10:19:04 +0300

    =============

--- Regular_Expressions.js
+++ Regular_Expressions.js
@@ -340,4 +340,20 @@ let result26 = favRegex.test(favWord);
 
 let sampleWord = "astronaut";
 let pwRegex = /(?=\w{5,})(?=\D\w*\d{2})/;
-let result26 = pwRegex.test(sampleWord);
+let result27 = pwRegex.test(sampleWord);
+
+
+
+
+
+
+//---------------------------------------------------------------------------------------------------
+
+//Fix the regex so that it checks for the names of Franklin Roosevelt or Eleanor 
+//Roosevelt in a case sensitive manner and it should make concessions for middle names.
+//Then fix the code so that the regex that you have created is checked against 
+//myString and either true or false is returned depending on whether the regex matches.
+
+let myString = "Eleanor Roosevelt";
+let myRegex =  /(Franklin ([A-Z]\. )?|Eleanor ([A-Z]\. )?)Roosevelt/;
+let result28 = myRegex.test(myString);
