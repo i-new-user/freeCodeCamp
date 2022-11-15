a3718be374a80b02abb5dc2881c288b13be98ae1
Stanislav <gsv9026@mail.ru>
2022-11-15 08:53:07 +0300

    Remove Items from an Array with pop() and shift()

--- BasicDataStructures.js
+++ BasicDataStructures.js
@@ -16,3 +16,14 @@ function mixedNumbers(arr) {
   }
   
 console.log(mixedNumbers(['IV', 5, 'six']));
+
+
+
+//Remove Items from an Array with pop() and shift()
+function popShift(arr) {
+    let popped = arr.pop();
+    let shifted = arr.shift(); 
+    return [shifted, popped];
+  }
+  
+console.log(popShift(['challenge', 'is', 'not', 'complete']));
