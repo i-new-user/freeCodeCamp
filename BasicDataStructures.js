3f581e0ab217cb56dd8e943a7ce095acbf5181d6
Stanislav <gsv9026@mail.ru>
2022-11-15 08:39:00 +0300

    Access an Arrays Contents Using Bracket Notation

--- BasicDataStructures.js
+++ BasicDataStructures.js
@@ -1,2 +1,8 @@
 //Use an Array to Store a Collection of Data
 let yourArray=['eeee', 33, true, null, 44];
+
+
+//Access an Array's Contents Using Bracket Notation
+let myArray = ["a", "b", "c", "d"];
+myArray[1] = 'sssss'
+console.log(myArray);
