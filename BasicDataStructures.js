8527b43b417a58b0f6b99a66841319c4b01dca10
Stanislav <gsv9026@mail.ru>
2022-11-16 08:31:04 +0300

    Iterate Through All an Arrays Items Using For Loops

--- BasicDataStructures.js
+++ BasicDataStructures.js
@@ -88,3 +88,20 @@ function quickCheck(arr, elem) {
 }
 
 console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+
+
+//Iterate Through All an Array's Items Using For Loops
+function filteredArray(arr, elem) {
+  let newArr = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf(elem) === -1) {
+      newArr.push(arr[i]);
+    }
+  }
+  
+  return newArr;
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
