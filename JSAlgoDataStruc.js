1cbaa7c0a31d0bad5b802ccde11dbd4b56e054b8
Stanislav <gsv9026@mail.ru>
2022-11-17 10:58:12 +0300

    Return Largest Numbers in Arrays

--- JSAlgoDataStruc.js
+++ JSAlgoDataStruc.js
@@ -33,3 +33,22 @@ function findLongestWordLength(str) {
 }
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+//Return Largest Numbers in Arrays
+function largestOfFour(arr) {
+  
+  var largest=[];
+   for(var i=0;i<arr.length;i++){
+    var gr=-500;
+    for(var j=0;j<=arr[i].length;j++){
+      if(arr[i][j]>gr){
+        gr=arr[i][j];
+      }
+    }
+    largest.push(gr);
+   }
+ return largest;
+}
+
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
