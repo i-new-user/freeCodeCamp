3e5675860911823e7431c6079bf2511c6edd7b60
Stanislav <gsv9026@mail.ru>
2022-11-17 10:35:21 +0300

    Find the Longest Word in a String

--- JSAlgoDataStruc.js
+++ JSAlgoDataStruc.js
@@ -19,3 +19,17 @@ function factorialize(num) {
 }
 
 factorialize(5);
+
+//Find the Longest Word in a String
+function findLongestWordLength(str) {
+  let strSpl = str.split(' ');
+  let zero = 0;
+  for(let i = 0; i < strSpl.length;i++){
+    if(strSpl[i].length > zero){
+        zero = strSpl[i].length;
+    }
+  }
+  return zero;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
