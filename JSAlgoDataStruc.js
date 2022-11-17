4992ef976a2bfb3f3a1ad361e153d439b14f5c63
Stanislav <gsv9026@mail.ru>
2022-11-17 11:16:08 +0300

    Repeat a String Repeat a String

--- JSAlgoDataStruc.js
+++ JSAlgoDataStruc.js
@@ -61,3 +61,19 @@ function confirmEnding(str, target) {
   }
   
   confirmEnding("Bastian", "n");
+
+
+//Repeat a String Repeat a String
+  function repeatStringNumTimes(str, num) {
+    if(num < 0){
+      return '';
+    }else{
+      let r = '';
+    for(let i = 0; i < num;i++){
+      r += str;
+    }
+    return r;
+    }
+  }
+  
+  repeatStringNumTimes("abc", 3);
