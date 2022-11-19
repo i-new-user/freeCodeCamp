8e915289db3ed8d75a1ea7ac8bde850fad9c6807
Stanislav <gsv9026@mail.ru>
2022-11-19 10:20:58 +0300

    Title Case a Sentence

--- JSAlgoDataStruc.js
+++ JSAlgoDataStruc.js
@@ -111,3 +111,15 @@ function confirmEnding(str, target) {
   }
   
   booWho(null);
+
+
+//Title Case a Sentence
+  function titleCase(str) {
+    let strArr = str.toLowerCase().split(' ');
+      for (var i = 0; i < strArr.length; i++) {
+          strArr[i] = strArr[i].charAt(0).toUpperCase() + strArr[i].slice(1);
+      }
+      return strArr.join(' ');
+}
+
+titleCase("I'm a little tea pot");
