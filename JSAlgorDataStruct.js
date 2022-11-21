fc9ded776eb59dcefd91d00456834294e3588a11
Stanislav <gsv9026@mail.ru>
2022-11-21 14:07:43 +0300

    Use Dot Notation to Access the Properties of an Object

--- JSAlgorDataStruct.js
+++ JSAlgorDataStruct.js
@@ -2,4 +2,13 @@
 let dog = {
     name: 'Bim',
     numLegs: 5
- };
+};
+
+//Use Dot Notation to Access the Properties of an Object
+let dog = {
+    name: "Spot",
+    numLegs: 4
+};
+  
+console.log(dog.numLegs)
+console.log(dog.name) 
