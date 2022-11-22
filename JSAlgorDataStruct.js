74416c9b2b9b9474b7718c8cb35b9d85e0ceeb57
Stanislav <gsv9026@mail.ru>
2022-11-22 08:24:20 +0300

    Iterate Over All Properties

--- JSAlgorDataStruct.js
+++ JSAlgorDataStruct.js
@@ -90,3 +90,25 @@ function Dog(name, color) {
   
   Dog.prototype.numLegs = 4;
   let beagle = new Dog("Snoopy");
+
+
+
+  //Iterate Over All Properties
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype.numLegs = 4;
+  
+  let beagle = new Dog("Snoopy");
+  
+  let ownProps = [];
+  let prototypeProps = [];
+  
+  for(let property in beagle){
+    if(beagle.hasOwnProperty(property)){
+      ownProps.push(property);
+    }else{
+      prototypeProps.push(property)
+    }
+  }
