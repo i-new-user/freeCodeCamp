2f168cd32ad730b845817bd647d2d020544ad18c
Stanislav <gsv9026@mail.ru>
2022-11-22 09:07:11 +0300

    Use Inheritance So You Dont Repeat Yourself

--- JSAlgorDataStruct.js
+++ JSAlgorDataStruct.js
@@ -179,3 +179,30 @@ Dog.prototype.isPrototypeOf(beagle);
 
 
 Object.prototype.isPrototypeOf(Dog.prototype);
+
+
+//Use Inheritance So You Don't Repeat Yourself
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+};
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
