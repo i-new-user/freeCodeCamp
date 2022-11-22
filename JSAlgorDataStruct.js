2bdeb030b763a764a0ce8ea2461d819af00e8904
Stanislav <gsv9026@mail.ru>
2022-11-22 08:16:23 +0300

    Use Prototype Properties to Reduce Duplicate Code

--- JSAlgorDataStruct.js
+++ JSAlgorDataStruct.js
@@ -80,3 +80,13 @@ function Dog(name, color) {
       ownProps.push(property);
     }
   }
+
+
+//Use Prototype Properties to Reduce Duplicate Code
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  
+  Dog.prototype.numLegs = 4;
+  let beagle = new Dog("Snoopy");
