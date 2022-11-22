89b8b27167892caf8bc80a84ac9a8d00ba590882
Stanislav <gsv9026@mail.ru>
2022-11-22 09:02:17 +0300

    Understand the Prototype Chain

--- JSAlgorDataStruct.js
+++ JSAlgorDataStruct.js
@@ -165,3 +165,17 @@ function Dog(name) {
 
 let beagle = new Dog("Snoopy");
 Dog.prototype.isPrototypeOf(beagle);
+
+
+
+//Understand the Prototype Chain
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);
+
+
+Object.prototype.isPrototypeOf(Dog.prototype);
