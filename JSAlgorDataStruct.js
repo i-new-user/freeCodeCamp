e59e7a50cbaeaa16079252e5e9299db1fd117b1b
Stanislav <gsv9026@mail.ru>
2022-11-23 08:59:41 +0300

    Add Methods After Inheritance

--- JSAlgorDataStruct.js
+++ JSAlgorDataStruct.js
@@ -254,3 +254,18 @@ let duck = new Bird();
 duck.constructor = Bird
 let beagle = new Dog();
 beagle.constructor = Dog
+
+
+
+//Add Methods After Inheritance
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+Dog.prototype = Object.create(Animal.prototype)
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function(){
+  console.log('Woof!');
+};
+
+let beagle = new Dog();
