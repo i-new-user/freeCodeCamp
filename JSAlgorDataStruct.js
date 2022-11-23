2c30dac49453683c4bbfe84f59a9573a3d620f1c
Stanislav <gsv9026@mail.ru>
2022-11-23 09:13:41 +0300

    Override Inherited Methods

--- JSAlgorDataStruct.js
+++ JSAlgorDataStruct.js
@@ -269,3 +269,22 @@ Dog.prototype.bark = function(){
 };
 
 let beagle = new Dog();
+
+
+//Override Inherited Methods
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+Penguin.prototype.fly = function() {
+  return 'Alas, this is a flightless bird.'; 
+};
+
+
+
+
+let penguin = new Penguin();
+console.log(penguin.fly());
