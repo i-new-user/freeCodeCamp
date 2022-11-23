77ccb2a6ca4ee21c34e1bd47b2d153cdad4a0438
Stanislav <gsv9026@mail.ru>
2022-11-23 08:28:38 +0300

    Inherit Behaviors from a Supertype

--- JSAlgorDataStruct.js
+++ JSAlgorDataStruct.js
@@ -206,3 +206,18 @@ Animal.prototype = {
     console.log("nom nom nom");
   }
 };
+
+
+//Inherit Behaviors from a Supertype
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+
+let duck = Object.create(Animal.prototype);
+let beagle = Object.create(Animal.prototype);
